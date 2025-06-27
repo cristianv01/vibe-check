2d81da2c3e078499fcbdb376c9ae00d4c2ab9a1b
Cristian Valencia <cristianv@vt.edu>
2025-06-26 21:25:54 -0400

    feature(auth): authentication for roles in not-corresponding roles page

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -4,9 +4,29 @@ import React from 'react'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
 import Navbar from '@/components/Navbar'
 import { useGetAuthUserQuery } from '@/state/api';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 const Layout = ({children}: {children: React.ReactNode}) => {
-  const {data: authUser} = useGetAuthUserQuery();
-  console.log("authUser", authUser);
+  const {data: authUser, isLoading: authLoading} = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname(); 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    if(authUser){
+        const userRole = authUser.userRole?.toLowerCase();
+        if(userRole === "owner" && pathname.startsWith("/search") ||
+        userRole === "owner" && pathname === '/'){
+            router.push("/owners/owned-properties", {scroll:false});
+        }
+    }else{
+        setIsLoading(false);
+    }
+   
+  }, [authUser,pathname,router])
+
+  if (authLoading || isLoading) return <div>Loading...</div>;
+
   return (
     <div className='h-full w-full'>
       <Navbar/>
